0ee642c1950ae1fe5917a0fc18af7accfc43df3a
otalorajuand <otalorajuand@gmail.com>
2022-10-25 18:25:08 +0000

    Addition of test file path

--- tests/test_models/test_file_storage.py
+++ tests/test_models/test_file_storage.py
@@ -7,6 +7,7 @@ from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from datetime import datetime
 import os
+import models
 
 
 class TestFileStorage(unittest.TestCase):
@@ -14,6 +15,11 @@ class TestFileStorage(unittest.TestCase):
     def test_file_path(self):
 
         file_1 = FileStorage()
+        self.assertTrue(file_1._FileStorage__file_path == 'file.json')
+
+    """
+    def test_objs(self):
+
         b_1 = BaseModel()
-        b_1.save()
-        self.assertTrue(os.path.exists('file.json'))
+        self.assertTrue(b_1 in models.storage.all())
+    """
