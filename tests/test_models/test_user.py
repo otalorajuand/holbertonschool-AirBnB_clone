1cf11a688ec11840a8e77f6a395b98ca5986f516
otalorajuand <otalorajuand@gmail.com>
2022-10-26 17:30:44 +0000

    Addition of tests for user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -19,19 +19,26 @@ class TestUser(unittest.TestCase):
         self.assertEqual(user_1.email, "otalorajuand@gmail.com") 
 
     def test_user_password(self):
+        
         user_1 = User()
-        User.password = "otalorajuand@gmail.com"
-        self.assertTrue(user_1.password == "otalorajuand@gmail.com")
-        self.assertTrue(type(User.password) is str)
+        self.assertTrue(hasattr(user_1, 'password'))
+        self.assertEqual(User.password, "")
+        user_1.password = "fsdfsdf"
+        self.assertEqual(user_1.password, "fsdfsdf") 
+
 
     def test_user_first_name(self):
+
         user_1 = User()
-        User.first_name = "otalorajuand@gmail.com"
-        self.assertTrue(user_1.first_name == "otalorajuand@gmail.com")
-        self.assertTrue(type(User.first_name) is str)
+        self.assertTrue(hasattr(user_1, 'first_name'))
+        self.assertEqual(User.first_name, "")
+        user_1.first_name = "alejo"
+        self.assertEqual(user_1.first_name, "alejo") 
 
     def test_user_last_name(self):
+    
         user_1 = User()
-        User.last_name = "otalorajuand@gmail.com"
-        self.assertTrue(user_1.last_name == "otalorajuand@gmail.com")
-        self.assertTrue(type(User.last_name) is str)
+        self.assertTrue(hasattr(user_1, 'last_name'))
+        self.assertEqual(User.last_name, "")
+        user_1.last_name = "garcia"
+        self.assertEqual(user_1.last_name, "garcia")
