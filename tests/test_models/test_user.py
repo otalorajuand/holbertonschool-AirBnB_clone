3324c71c81bdf4bbb5425bd19d3704742aec039f
otalorajuand <otalorajuand@gmail.com>
2022-10-26 17:11:37 +0000

    Addition of tests for user

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Unittest for class Usert
+Unittest for class User
 """
 import unittest
 from models.user import User
@@ -14,3 +14,22 @@ class TestUser(unittest.TestCase):
         user_1 = User()
         User.email = "otalorajuand@gmail.com"
         self.assertTrue(user_1.email == "otalorajuand@gmail.com")
+        self.assertTrue(type(User.email) is str)
+
+    def test_user_password(self):
+        user_1 = User()
+        User.password = "otalorajuand@gmail.com"
+        self.assertTrue(user_1.password == "otalorajuand@gmail.com")
+        self.assertTrue(type(User.password) is str)
+
+    def test_user_first_name(self):
+        user_1 = User()
+        User.first_name = "otalorajuand@gmail.com"
+        self.assertTrue(user_1.first_name == "otalorajuand@gmail.com")
+        self.assertTrue(type(User.first_name) is str)
+
+    def test_user_last_name(self):
+        user_1 = User()
+        User.last_name = "otalorajuand@gmail.com"
+        self.assertTrue(user_1.last_name == "otalorajuand@gmail.com")
+        self.assertTrue(type(User.last_name) is str)
