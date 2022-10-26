eef2a16f49499bf9875682009c2195269910cab2
otalorajuand <otalorajuand@gmail.com>
2022-10-26 21:17:35 +0000

    Addition of reload test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -36,6 +36,23 @@ class TestFileStorage(unittest.TestCase):
         file_1.save()
         self.assertTrue(file_1._FileStorage__objects.get(f"BaseModel.{obj_1.id}"))
 
+    
+    def test_reload(self):
+        """
+        Tests method: reload (reloads objects from string file)
+        """
+        a_storage = FileStorage()
+        try:
+            os.remove("file.json")
+        except:
+            pass
+        with open("file.json", "w") as f:
+            f.write("{}")
+        with open("file.json", "r") as r:
+            for line in r:
+                self.assertEqual(line, "{}")
+        self.assertIs(a_storage.reload(), None)
+
     """
     def test_reload(self):
 
