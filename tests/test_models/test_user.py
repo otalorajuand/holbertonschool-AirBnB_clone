caf09310db1d6d491e3b6b59911db3506e455384
otalorajuand <otalorajuand@gmail.com>
2022-10-26 17:04:27 +0000

    Addition of tests for usert

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Unittest for class Usert
+"""
+import unittest
+from models.user import User
+from datetime import datetime
+import os
+
+
+class TestUser(unittest.TestCase):
+    
+    def test_user_email(self):
+        user_1 = User()
+        user_1.email = "otalorajuand@gmail.com"
+        self.assertTrue(user_1.email == "otalorajuand@gmail.com")
