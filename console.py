298db37a91232fb8e103ab14bfd049f24f3c711c
alejandrodgz <alejandrodgz@github.com>
2022-10-26 09:07:28 -0500

    task 8

--- console.py
+++ console.py
@@ -24,7 +24,7 @@ class HBNBCommand(cmd.Cmd):
         elif line_read[0] not in ["BaseModel", "User"]:
             print("** class doesn't exist **")
         else:
-            b1 = eval(f"models.{class_id[line_read[0]]}{line_read[0]}()")
+            b1 = eval(f"models.{class_id[line_read[0]]}.{line_read[0]}()")
             b1.save()
             print(b1.id)
     
