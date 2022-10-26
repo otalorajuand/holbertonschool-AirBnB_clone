fa6cf054c3fa73b23b8926ccd42cf89626073359
otalorajuand <otalorajuand@gmail.com>
2022-10-26 16:41:38 +0000

    Addition of tests reaload

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -44,5 +44,5 @@ class TestFileStorage(unittest.TestCase):
         file_1.save()
         file_1._FileStorge__objects = {}
         file_1.reload()
-        self.assertTrue(file_1._FileStorage__objects != {})
+        self.assertTrue(file_1._FileStorage__objects.get(f"BaseModel.{obj_1.id}")))
 
